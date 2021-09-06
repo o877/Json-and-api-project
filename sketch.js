60fdf40002117ba14018ef01bd5b51327c3d002e
Om Gain <78549861+o877@users.noreply.github.com>
2021-09-06 19:46:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,11 +7,11 @@ var engine, world;
 var backgroundImg;
 var hour;
 
-var bg = "sunset.png";
+var bg = "sunrise1.png";
 
 function preload() {
+    // create getBackgroundImg( ) here
     getBackgroundImg();
-    
 }
 
 function setup(){
@@ -22,43 +22,43 @@ function setup(){
 }
 
 function draw(){
-    // add condition to check if any background image is there to add
-   if(backgroundImg)
-   background(backgroundImg);
+     // add condition to check if any background image is there to add
+    if(backgroundImg)
+    background(backgroundImg);
 
-   Engine.update(engine);
-   // write code to display time in correct format here
-   fill("black");
-   textSize(30);
+    Engine.update(engine);
+    // write code to display time in correct format here
+    fill("black");
+    textSize(30);
 
-   if(hour>=12){
-       text("Time : "+ hour%12 + " PM", 50,100);
-      }else if(hour==0){
-        text("Time : 12 AM",100,100);
-      }else{
-       text("Time : "+ hour%12 + " AM", 50,100);
-      }
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+       }else if(hour==0){
+         text("Time : 12 AM",100,100);
+       }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
+       }
 
 }
 
 async function getBackgroundImg(){
 
-   // write code to fetch time from API
-   var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    // write code to fetch time from API
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
 
-   //change the data in JSON format
-   var responseJSON = await response.json();
-   var datetime = responseJSON.datetime;
-   
-   // write code slice the datetime
-   hour = datetime.slice(11,13);
+    //change the data in JSON format
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+    
+    // write code slice the datetime
+    hour = datetime.slice(11,13);
 
-   if(hour>=0 && hour<18 ){
-       bg = "sunrise.png";
-   }
-   else{
-       bg="sunset.png"
-   }
-   
-   backgroundImg = loadImage(bg);
+    if(hour>=0 && hour<18 ){
+        bg = "sunrise.png";
+    }
+    else{
+        bg="sunset.png"
+    }
+    
+    backgroundImg = loadImage(bg);
 }
